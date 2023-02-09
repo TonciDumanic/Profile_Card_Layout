28430fa763f1ca91d498d2206f8938a71c6ae422
etondum <tonci.dumanic@ericsson.com>
2023-02-09 10:08:03 +0100

    Added LazyList and Coil with new images an user examples downloaded from the internet.

--- app/src/main/java/hr/dumanic/tonci/profilecardlayout/MainActivity.kt
+++ app/src/main/java/hr/dumanic/tonci/profilecardlayout/MainActivity.kt
@@ -6,66 +6,101 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import hr.dumanic.tonci.profilecardlayout.ui.theme.ProfileCardLayoutTheme
+import hr.dumanic.tonci.profilecardlayout.ui.theme.lightGreen
+@coil.annotation.ExperimentalCoilApi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ProfileCardLayoutTheme {
 
-                    MainScreen("Android")
+                MainScreen()
+            }
 
         }
     }
 }
+@coil.annotation.ExperimentalCoilApi
 
 @Composable
-fun MainScreen(name: String) {
-    Surface(modifier = Modifier.fillMaxSize(),color = Color.LightGray) {
-        ProfileCard()
+fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
+
+    Scaffold(topBar = { AppBar() }) {  padding ->
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)) {
+            LazyColumn() {
+
+                items(userProfiles) { userProfiles ->
+                    ProfileCard(userProfile = userProfiles)
+                }
+
+            }
+
+        }
     }
+
 }
+@Composable
+fun AppBar() {
+    TopAppBar (navigationIcon = { Icon(Icons.Default.Home, "content Description", Modifier.padding(12.dp)) },
+        title = { Text("Messiging Application users")}    )
+}
+@coil.annotation.ExperimentalCoilApi
 
 @Composable
-fun ProfileCard() {
+fun ProfileCard(userProfile:UserProfile) {
     Card(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.Top),
-        elevation = 8.dp) {
+        elevation = 8.dp,
+        backgroundColor = Color.White) {
 
        Row(
            modifier = Modifier.fillMaxWidth(),
            verticalAlignment = Alignment.CenterVertically,
            horizontalArrangement = Arrangement.Start ) {
-           ProfilePicture()
-           ProfileContent()
+           ProfilePicture(userProfile.pictureUrl,userProfile.status)
+           ProfileContent(userProfile.name,userProfile.status)
        }
     }
 }
 @Composable
-fun ProfilePicture() {
+@coil.annotation.ExperimentalCoilApi
+fun ProfilePicture(pictureUrl :String,onlineStatus : Boolean) {
     Card(
         shape = CircleShape,
-        border = BorderStroke(width = 2.dp,color = Color.Green),
+        border = BorderStroke(width = 2.dp,
+            color = if(onlineStatus)
+                        MaterialTheme.colors.lightGreen
+                else
+                    Color.Red ),
         modifier = Modifier.padding(16.dp),
         elevation = 4.dp
     ) {
 
         Image(
-            painter = painterResource(id = R.drawable.mojaslika),
+
+            painter =rememberImagePainter(pictureUrl),
             contentDescription = "Content description",
             Modifier.size(72.dp),
             contentScale = ContentScale.Crop)
@@ -73,19 +108,28 @@ fun ProfilePicture() {
     }}
 
 @Composable
-fun ProfileContent() {
+fun ProfileContent(userName: String,onlineStatus : Boolean) {
 
     Column(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()) {
-        Text(
-            text = "Tonči Dumanić",
-            style = MaterialTheme.typography.h5
-        )
+        CompositionLocalProvider( LocalContentAlpha provides if(onlineStatus) ContentAlpha.medium else ContentAlpha.disabled
+
+         )
+        {
+
+            Text(
+                text = userName,
+                style = MaterialTheme.typography.h5
+            )
+        }
         CompositionLocalProvider( LocalContentAlpha provides ContentAlpha.medium) {
             Text(
-                text = "Active now",
+                text = if(onlineStatus)
+                            "Active now"
+                        else
+                            "Offline",
                 style = MaterialTheme.typography.body2
 
             )
@@ -95,10 +139,11 @@ fun ProfileContent() {
     }
 
 }
+@coil.annotation.ExperimentalCoilApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ProfileCardLayoutTheme {
-        MainScreen("Android")
+        MainScreen()
     }
 }
